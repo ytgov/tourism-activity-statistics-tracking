ce4d9e0dc1814accbfa13ee1e07ae2026b8da1e6
Michael Johnson <michael@icefoganalytics.com>
2023-03-07 10:27:14 -0700

    Update 002_create-visitor_centre.ts

--- src/api/src/data/migrations/002_create-visitor_centre.ts
+++ src/api/src/data/migrations/002_create-visitor_centre.ts
@@ -1,9 +1,9 @@
 import * as knex from "knex";
 
-exports.up = function (knex: knex.Knex, Promise: any) {
+exports.up = async function (knex: knex.Knex, Promise: any) {
   console.log("Created visitor_centre table");
 
-  return knex.schema.createTable("visitor_centre", function (t) {
+  await knex.schema.createTable("visitor_centre", function (t) {
     t.increments("id").primary();
     t.string("name", 200).notNullable().unique();
     t.string("community", 200).notNullable();
@@ -11,6 +11,20 @@ exports.up = function (knex: knex.Knex, Promise: any) {
     t.boolean("is_active").notNullable().defaultTo(true);
     t.integer("sort_order").notNullable().defaultTo(10);
   });
+
+
+  let site_array = [
+    {"site_id":1,"site_desc":"Whitehorse VIC","is_active_flg":"Yes","rec_create_date":"16-MAY-13","rec_create_user":0}
+    ,{"site_id":3,"site_desc":"Haines Junction VIC","is_active_flg":"Yes","rec_create_date":"16-MAY-13","rec_create_user":0}
+    ,{"site_id":4,"site_desc":"Beaver Creek VIC","is_active_flg":"Yes","rec_create_date":"16-MAY-13","rec_create_user":0}
+    ,{"site_id":5,"site_desc":"Old Crow VIC","is_active_flg":"Yes","rec_create_date":"16-MAY-13","rec_create_user":0}
+    ,{"site_id":6,"site_desc":"Carcross VIC","is_active_flg":"Yes","rec_create_date":"16-MAY-13","rec_create_user":0}
+    ,{"site_id":7,"site_desc":"Airport","is_active_flg":"Yes","rec_create_date":"16-MAY-13","rec_create_user":0}
+    ,{"site_id":8,"site_desc":"Dawson VIC","is_active_flg":"Yes","rec_create_date":"16-MAY-13","rec_create_user":0}
+    ,{"site_id":2,"site_desc":"Watson Lake VIC","is_active_flg":"Yes","rec_create_date":"16-MAY-13","rec_create_user":0}
+    ]
+
+  return knex("visitor_center").insert(site_array)
 };
 
 exports.down = function (knex: knex.Knex, Promise: any) {
